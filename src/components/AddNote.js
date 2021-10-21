1f0604a9cd32adf113c6158ef9cf06aadefcf589
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2021-10-21 21:53:36 +0530

    Removed unnecessary

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const AddNote = ({ handleAddNote }) => {
 	const [noteText, setNoteText] = useState('');
-	const characterLimit = 200;
+	const characterLimit = 251;
 
 	const handleChange = (event) => {
 		if (characterLimit - event.target.value.length >= 0) {
